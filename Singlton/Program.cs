b119c314ed853e444058fc63c805a4cd88afb152
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-07-21 22:14:30 +0600

    Добавьте файлы проекта.

---
+++ Singlton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singlton
+{
+    class Singlton
+    {
+        static Singlton uniqInstance;
+        protected Singlton()
+        {
+        }
+
+        public static Singlton Instance()
+        {
+            if (uniqInstance==null)
+            {
+                uniqInstance = new Singlton();
+            }
+            return uniqInstance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singlton singlton1 = Singlton.Instance();
+            Singlton singlton2 = Singlton.Instance();
+            if (ReferenceEquals(singlton1,singlton2))
+            {
+                Console.WriteLine("объект Одиночка работает");
+            }
+            else
+            {
+                Console.WriteLine("объект Одиночка не работает");
+            }
+        }
+    }
+}
